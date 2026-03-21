0a899fe9abb90e265aaf2e8c67df71e882a25091
cwaidner64 <cwaidner64@gmail.com>
2026-03-21 09:04:28 -0400

    fix base

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   assetsInclude: ["**/*.php"],
-  base: "/cwaidner64.datacrawl.io/",
+  base: "/datacrawl.github.io/",
 });
